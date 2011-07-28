56c0ce92ff6cdbb69941f46dbf26b45ee537f4ff
Dave Steele <daves@satellite.(none)>
2011-07-28 19:34:32 -0400

    Made changes to jgarzik's python-bitcoinrpc to eliminate json 'Decimal' error

--- jsonrpc/json.py
+++ jsonrpc/json.py
@@ -1,4 +1,7 @@
-json = __import__('json')
+try:
+    json = __import__('simplejson')
+except ImportError:
+    json = __import__('json')
 loads = json.loads
 dumps = json.dumps
 JSONEncodeException = TypeError
